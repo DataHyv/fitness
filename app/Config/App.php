491a7cc73843f5f81f9aad26eacb50cace83f4e5
ericogalileo <erick@datahyv.com>
2022-02-11 13:25:56 +0800

    Fix the 404 in login and signup

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/fitness/';
+    public $baseURL = 'http://localhost:80/fitness/';
 
     /**
      * --------------------------------------------------------------------------
